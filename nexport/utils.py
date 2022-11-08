f1555c9c91106ebe7eb7d1f7671a12f0c0cf0c26
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2022-11-08 14:18:44 -0800

    refactor: Edited colors.py import for higher-level color access.

--- nexport/utils.py
+++ nexport/utils.py
@@ -3,18 +3,18 @@ from torch import nn
 import datetime as dt
 
 # File imports
-import colors as c
+from colors import color as c
 
 
 # Module functions
 
 def export_to_file(model: object, filename: str):
     """Function which exports all weight and bias arrays to a file."""
-    print(f"Creating file: {c.color.YELLOW}{filename}{c.color.DEFAULT}")
+    print(f"Creating file: {c.YELLOW}{filename}{c.DEFAULT}")
     f = open(filename, "w")
     for x, y in enumerate(model.parameters()): # access parameter array
         if (x % 2 != 1): # if array is not odd (even index = weights)
-            print(f"Extracting {c.color.GREEN}weights{c.color.DEFAULT} from layer {c.color.RED}{(int(x/2))+1}{c.color.DEFAULT}")
+            print(f"Extracting {c.GREEN}weights{c.DEFAULT} from layer {c.RED}{(int(x/2))+1}{c.DEFAULT}")
             for i, z in enumerate(y): # access weight array x in layer y
                 if i == 0:
                     f.write("[")
@@ -30,15 +30,15 @@ def export_to_file(model: object, filename: str):
                 f.write("\n")
             f.write("\n")
         else: # if array is odd (odd index = biases)
-            print(f"Extracting {c.color.MAGENTA}biases{c.color.DEFAULT} from layer  {c.color.RED}{(int(x/2))+1}{c.color.DEFAULT}")
+            print(f"Extracting {c.MAGENTA}biases{c.DEFAULT} from layer  {c.RED}{(int(x/2))+1}{c.DEFAULT}")
             biases = []
             for b in y:
                 biases.append(float(b))
             f.write(str(biases))
             f.write("\n\n\n")
-    print(f"Saving file: {c.color.YELLOW}{filename}{c.color.DEFAULT}")
+    print(f"Saving file: {c.YELLOW}{filename}{c.DEFAULT}")
     f.close()
-    print(f"{c.color.CYAN}Done!{c.color.DEFAULT}")
+    print(f"{c.CYAN}Done!{c.DEFAULT}")
 
 
 def calculate_params(model: object, param_type: str = "wb"):
