8ac4bff5f602aea87f4e37f8bb5380c1944c9a42
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-10 18:02:11 -0800

    test: Added test case for .txt export.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import nexport
 
 model = nexport.FFNetwork()
-nexport.export_to_json(model, verbose=1, include_metadata=False)
+nexport.export_to_json(model, indent=2, verbose=1, include_metadata=False)
+# nexport.export_to_file(model, filename="model")
