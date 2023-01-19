f4ce8667ddd9c368f3d03c0c9f3982ea484202f3
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-18 16:15:56 -0800

    refac: Elevated calculator functions to module-level.

--- nexport/__init__.py
+++ nexport/__init__.py
@@ -1,7 +1,8 @@
 # Import utils.py so module functions are
 # usable at 'import nexport' level.
+from .calculators import *
 from .utils import *
 
 # Only show functions specified in
 # utils.__all__ to the outside world.
-__all__ = utils.__all__
+__all__ = utils.__all__, calculators.__all__
