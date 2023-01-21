a4d280fce39057b06b83764700c9328daf7d0d6e
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-20 21:17:18 -0800

    feat: Added verbose error handling with exceptions in export function.

--- nexport/utils.py
+++ nexport/utils.py
@@ -48,9 +48,13 @@ def export(model: object, filetype: str, filename: str = "model", indent: int = 
                     npte.export_to_file(model=model, filename=filename)
                 case "json":
                     npte.export_to_json(model=model, filename=filename, indent=indent, verbose=verbose, include_metadata=include_metadata, model_name=model_name, model_author=model_author)
+                case "csv", "xml":
+                    raise NotImplementedError(f"This feature ({filetype} for {nexport.__framework__}) has not yet been implemented.")
+                case other:
+                    raise RuntimeError(f"This filetype ({other}) is unrecognized and will not be supported in the near future.")
         case "tensorflow":
             match filetype:
-                case "txt":
-                    print("This feature is coming soon...")
-                case "json":
-                    print("This feature is coming soon...")
+                case "txt", "json", "csv", "xml":
+                    raise NotImplementedError(f"This feature ({filetype} for {nexport.__framework__}) has not yet been implemented.")
+                case other:
+                    raise RuntimeError(f"This filetype ({other}) is unrecognized and will not be supported in the near future.")
