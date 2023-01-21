51b01f1fec76060480582122899a9368781136bc
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-20 21:23:30 -0800

    fix: Caught and fixed bug which outputted wrong error for unsupported filetype export.

--- nexport/utils.py
+++ nexport/utils.py
@@ -48,13 +48,13 @@ def export(model: object, filetype: str, filename: str = "model", indent: int = 
                     npte.export_to_file(model=model, filename=filename)
                 case "json":
                     npte.export_to_json(model=model, filename=filename, indent=indent, verbose=verbose, include_metadata=include_metadata, model_name=model_name, model_author=model_author)
-                case "csv", "xml":
+                case "csv" | "xml":
                     raise NotImplementedError(f"This feature ({filetype} for {nexport.__framework__}) has not yet been implemented.")
                 case other:
                     raise RuntimeError(f"This filetype ({other}) is unrecognized and will not be supported in the near future.")
         case "tensorflow":
             match filetype:
-                case "txt", "json", "csv", "xml":
+                case "txt" | "json" | "csv" | "xml":
                     raise NotImplementedError(f"This feature ({filetype} for {nexport.__framework__}) has not yet been implemented.")
                 case other:
                     raise RuntimeError(f"This filetype ({other}) is unrecognized and will not be supported in the near future.")
