04d77ce7c162b58e852a02524fdfc75490298d7a
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-24 13:59:35 -0800

    fix: Fixed broken function 'import_from_file()'.

--- nexport/pytorch/importing.py
+++ nexport/pytorch/importing.py
@@ -1,6 +1,7 @@
 # Module imports
 import numpy as np
-import torch.nn
+import torch
+from torch import nn
 
 # External function visibility
 __all__ = ['import_from_file']
@@ -55,3 +56,68 @@ def import_from_file(filepath: str) -> object:
     for x, layer in enumerate(model_biases):
         model_biases[x] = np.array(layer)
         model_biases[x] = model_biases[x].astype(np.float)
+
+    class InputBlock(nn.Module):
+        def __init__(self, weights: list, biases: list):
+            super(InputBlock, self).__init__()
+            
+            self.input_layer = nn.Linear(10, 64)
+            self.activation = nn.Sigmoid()
+            
+            self.input_layer.weight.data = torch.Tensor(np.transpose(np.array(weights)))
+            self.input_layer.bias.data = torch.Tensor(biases)
+            
+            
+        def forward(self, input):
+            return self.activation(self.input_layer(input))
+
+    class ComputeBlock(nn.Module):
+        def __init__(self, weights: list, biases: list):
+            super(ComputeBlock, self).__init__()
+            
+            self.comp_layer = nn.Linear(64, 64)
+            self.activation = nn.Sigmoid()
+            
+            self.comp_layer.weight.data = torch.Tensor(weights)
+            self.comp_layer.bias.data = torch.Tensor(biases)
+            
+        def forward(self, input):
+            return self.activation(self.comp_layer(input))
+
+    class OutputBlock(nn.Module):
+        def __init__(self, weights: list, biases: list):
+            super(OutputBlock, self).__init__()
+            
+            self.output_layer = nn.Linear(64, 1)
+            self.activation = nn.Sigmoid()
+            
+            self.output_layer.weight.data = torch.Tensor(np.transpose(np.array(weights)))
+            self.output_layer.bias.data = torch.Tensor(biases)
+            
+            
+        def forward(self, input):
+            return self.activation(self.output_layer(input))
+
+    class Model(nn.Module):
+        def __init__(self, num_blocks):
+            super(Model, self).__init__()
+            self.num_blocks = num_blocks
+            
+            self.input_block = InputBlock(weights = model_weights[0], biases = model_biases[0])
+            
+            self.compute_block = nn.ModuleList([
+                ComputeBlock(weights = model_weights[x+1], biases = model_biases[x+1])
+                for x in range(num_blocks)
+            ])
+            self.output_block = OutputBlock(weights = np.array(model_weights[-1]), biases = np.array(model_biases[-1]))
+        
+        
+        def forward(self, input):
+            output = self.input_block(input)
+            for x in range(50):  
+                output = self.compute_block[x](output)
+            output = self.output_block(output)
+            return output
+
+    network = Model(50)
+    return network
