d515bc8945266766f18926b21b52352480a653fa
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-30 23:23:35 -0800

    feat: Added XORNetwork to sample models.

--- nexport/models.py
+++ nexport/models.py
@@ -1,8 +1,9 @@
 # Module imports
+import torch
 from torch import nn
 
 # External function visibility
-__all__ = ['FFNetwork', 'BFNetwork', 'ICARNetwork']
+__all__ = ['FFNetwork', 'BFNetwork', 'ICARNetwork', 'XORNetwork']
 
 
 # Model classes
@@ -146,3 +147,64 @@ class ICARNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(64, 10)
         )
+
+
+class XORNetwork(nn.Module):
+    def __init__(self):
+        super(XORNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        
+        firstlayer = nn.Linear(2, 3)
+        firstlayer.weight.data = XORNetwork.first_weights
+        firstlayer.bias.data = XORNetwork.first_biases
+        
+        secondlayer = nn.Linear(3, 3)
+        secondlayer.weight.data = XORNetwork.second_weights
+        secondlayer.bias.data = XORNetwork.second_biases
+        
+        thirdlayer = nn.Linear(3, 1)
+        thirdlayer.weight.data = XORNetwork.third_weights
+        thirdlayer.bias.data = XORNetwork.third_biases
+        
+        self.linear_step_stack = nn.Sequential(
+            firstlayer,
+            XORNetwork.StepHS(),
+            secondlayer,
+            XORNetwork.StepHS(),
+            thirdlayer,
+            XORNetwork.StepHS()
+        )
+
+    # Network parameters
+    ## Hidden layer 1
+    first_weights = torch.tensor([[1.0, 0.0],
+                                [1.0, 1.0],
+                                [0.0, 1.0]])
+    first_biases = torch.tensor([0.0, -1.99, 0.0])
+
+    ## Hidden layer 2
+    second_weights = torch.tensor([[1.0, 0.0, 0.0],
+                                [0.0, 1.0, 0.0],
+                                [0.0, 0.0, 1.0]])
+    second_biases = torch.tensor([0.0, 0.0, 0.0])
+
+    ## Output layer
+    third_weights = torch.tensor([[1.0, -2.0, 1.0]])
+    third_biases = torch.tensor([0.0])
+
+    # Binary step activation function
+    class StepHS(nn.Module):
+        def __init__(self):
+            super().__init__()
+            
+        def forward(self, input):
+            output = torch.heaviside(input, torch.zeros(input.shape[0]))
+            return torch.Tensor(output)
+        
+    # Forward function (matmul with activation function)
+    def forward(self, input):
+        return self.linear_step_stack(input)
+
+    
+        
+
