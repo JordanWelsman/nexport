07539570828c6a3d207b03bf9c31683fcff490b9
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-02-03 17:54:31 -0800

    build: Incremented version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 # Arguments
-version = "0.4.0" # update __init__.py
+version = "0.4.1" # update __init__.py
 python_version = ">=3.10"
 
 # Long description from README.md
